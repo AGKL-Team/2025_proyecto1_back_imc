e732d670a91fbe0675510d6136b2fa83434c137e
ValeLattanzi <vale.lattanzi15@gmail.com>
2025-09-09 21:32:54 -0300

    feat: joi adde

---
+++ src/config/config.schema.ts
@@ -0,0 +1,9 @@
+import * as Joi from 'joi';
+
+export const configSchema = Joi.object({
+  SUPABASE_URL: Joi.string().uri().required(),
+  SUPABASE_KEY: Joi.string().required(),
+  FRONTEND_URL: Joi.string().uri().required(),
+  DATABASE_URL: Joi.string().uri().required(),
+  JWT_SECRET: Joi.string().required(),
+});
--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import frontentConfig from './frontent.config';
+import supabaseConfig from './supabase.config';
+import { configSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-      // load: [frontentConfig, supabaseConfig],
+      load: [frontentConfig, supabaseConfig],
+      validationSchema: configSchema,
     }),
   ],
 })
