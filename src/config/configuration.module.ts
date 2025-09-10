c0815a1bb56aebe67c4fa44ff8ca3c8234ab2ebd
ValeLattanzi <vale.lattanzi15@gmail.com>
2025-09-09 21:34:15 -0300

    fix: ommit env file on prod

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { configSchema } from './config.schema';
 import frontentConfig from './frontent.config';
 import supabaseConfig from './supabase.config';
-import { configSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
       envFilePath: '.env',
       load: [frontentConfig, supabaseConfig],
       validationSchema: configSchema,
