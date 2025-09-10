2f37c6728c4d4dba1ae01ce14c539402f0afa93b
ValeLattanzi <vale.lattanzi15@gmail.com>
2025-09-09 21:25:06 -0300

    test: disable modules

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import frontentConfig from './frontent.config';
-import supabaseConfig from './supabase.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-      load: [frontentConfig, supabaseConfig],
+      // load: [frontentConfig, supabaseConfig],
     }),
   ],
 })
