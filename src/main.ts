b02797facdd4dfb43fca323d641cd939b68fbf3f
ValeLattanzi <vale.lattanzi15@gmail.com>
2025-09-10 15:36:38 -0300

    fix: package-lock.json removed

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { Handler } from 'express';
 import { AppModule } from './application/app.module';
 
-const server = express();
-let cachedApp: any;
-
 async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(server),
-      {
-        logger: ['log', 'error', 'warn', 'debug', 'verbose'],
-      },
-    );
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
-    // Middleware / pipes globales
-    app.enableCors({
-      origin: process.env.FRONTEND_URL || '*',
-    });
-    app.useGlobalPipes(
-      new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-    );
+  // Middleware / pipes globales
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
-    // Inicialización de la app (no listen)
-    await app.init();
-    cachedApp = app;
-  }
-  return cachedApp;
+  // Inicialización de la app (no listen)
+  await app.init();
+  await app.listen(3000);
 }
 
-// Vercel handler
-export default server as Handler;
-
-// Inicializamos la app en cold-start
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
